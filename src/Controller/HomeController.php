00283e63d64068db6a0642c63704588c7a347f23
lmay <may.laurent@gmail.com>
2020-07-07 18:08:29 +0400

    Routes paramétrées & routes multiples

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
+    /**
+     * @Route("/hello/{prenom}", name="hello")
+     * @Route("/hello", name="hello_base}
+     * Montre la page qui dit bonjour
+     *
+     * @return Response
+     */
+    public function hello($prenom = anonyme) {
+        return new Response("Bonjour " . $prenom);
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function home()
     {
+        $prenoms = ["Laurent" => 31, "Fredo" => 26, "Alexis" => 12];
         return $this->render(
-            'home.html.twig'
+            'home.html.twig',
+            [
+                'title' => "Bonjour à tous",
+                'age' => 31,
+                'tableau' => $prenoms
+            ]
         );
     }
 }
