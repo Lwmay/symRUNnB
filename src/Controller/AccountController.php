09287a549f204d1954f47abd8d55556f864740c1
Lwmay <45488236+Lwmay@users.noreply.github.com>
2020-08-31 09:59:49 +0400

    Gestion des utilisateurs et liaison avec les annonces.

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PasswordUpdate;
+use App\Entity\User;
+use App\Form\AccountType;
+use App\Form\PasswordUpdateType;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AccountController extends AbstractController
+{
+    /**
+     * Permet de se connecter
+     *
+     * @Route("/login", name="account_login")
+     *
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * Permet de se déconnecter
+     *
+     * @Route("/logout", name="account_logout")
+     *
+     * @return void
+     */
+    public function logout()
+    {
+        // ... rien
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'inscription
+     *
+     * @Route("/register", name="account_register")
+     *
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $hash = $encoder->encodePassword($user, $user->getHash());
+            $user->setHash($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash('success', "Votre compte a bien été créé.");
+
+            return $this->redirectToRoute("account_login");
+        }
+
+        return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher et de traiter le formulaire de modification de profil
+     *
+     * @Route ("/account/profile", name="account_profile")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function profile(Request $request, EntityManagerInterface $manager)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash("success", "Modifications enregistrées.");
+        }
+
+        return $this->render('account/profile.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet de modifier le mot de passe
+     *
+     * @Route("/account/password-update", name="account_password"))
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function updatePassword(Request $request,
+                                   UserPasswordEncoderInterface $encoder,
+                                   EntityManagerInterface $manager )
+        {
+        $passwordUpdate = new PasswordUpdate();
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 1.Vérifier que le oldPassword est du formulaire soit le même que le password de l'user.
+
+            if (!password_verify($passwordUpdate->getOldPassword(), $user->getHash())){
+
+                //Gérer l'erreur
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe n'est pas valide."));
+
+            } else {
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+
+                $user->setHash($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Votre mot de passe à bien été modifié'
+                );
+
+                return $this->redirectToRoute('homepage');
+            }
+        }
+
+        return $this->render("account/password.html.twig", [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afiicher le profil d'un uilisateur connecté
+     *
+     * @Route("/account", name="account_index")
+     *
+     * @return Response
+     */
+    public function myAccount() {
+        return $this->render('user/index.html.twig', [
+            'user' => $this->getUser()
+        ]);
+    }
+}
